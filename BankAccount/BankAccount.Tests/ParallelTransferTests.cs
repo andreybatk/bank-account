07f80024090174f885ebcd74cf362b8cce8ff836
DESKTOP-85SG08H\AB <batyhtik3@mail.ru>
2025-08-12 02:29:34 +0300

    feat: added integration tests, parallel transfer tests

---
+++ BankAccount/BankAccount.Tests/ParallelTransferTests.cs
@@ -0,0 +1,90 @@
+﻿using BankAccount.BusinessLogic.Accounts.Commands;
+using BankAccount.Domain;
+using BankAccount.Domain.Enums;
+using System.Net.Http.Json;
+
+namespace BankAccount.Tests;
+
+public class ParallelTransferTests(TestApplicationFactory factory)
+    : IClassFixture<TestApplicationFactory>, IAsyncLifetime
+{
+    private readonly HttpClient _client = factory.CreateClient();
+
+    private Guid _accountFromId;
+    private Guid _accountToId;
+
+    public async Task InitializeAsync()
+    {
+        await InitializeAccountsAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task ParallelTransfers_ShouldPreserveTotalBalance()
+    {
+        // arrange
+        var accountFromId = await CreateTestAccountAsync(1000m);
+        var accountToId = await CreateTestAccountAsync(1000m);
+
+        var initialTotal = await GetBalanceAsync(accountFromId) + await GetBalanceAsync(accountToId);
+
+        // act
+        var tasks = Enumerable.Range(0, 50).Select(_ =>
+            _client.PostAsJsonAsync("/api/transactions/transfers", new
+            {
+                FromAccountId = accountFromId,
+                ToAccountId = accountToId,
+                Amount = 10m
+            })
+        );
+
+        await Task.WhenAll(tasks);
+
+        // assert
+        var finalTotal = await GetBalanceAsync(accountFromId) + await GetBalanceAsync(accountToId);
+        Assert.Equal(initialTotal, finalTotal);
+    }
+
+    private async Task InitializeAccountsAsync()
+    {
+        _accountFromId = await CreateTestAccountAsync(initialBalance: 1000m);
+        _accountToId = await CreateTestAccountAsync(initialBalance: 1000m);
+    }
+
+    private async Task<Guid> CreateTestAccountAsync(decimal initialBalance)
+    {
+        var command = new CreateAccountCommand(
+            OwnerId: Guid.NewGuid(),
+            Type: EAccountType.Checking,
+            Currency: "RUB",
+            InitialBalance: initialBalance,
+            InterestRate: null,
+            OpenDate: DateTime.UtcNow,
+            CloseDate: null
+        );
+
+        var response = await _client.PostAsJsonAsync("/api/accounts", command);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadFromJsonAsync<MbResult<Guid>>();
+        if (result is null)
+            throw new InvalidOperationException("Ошибка при создании клиента в CreateTestAccountAsync.");
+
+        return result.Value;
+    }
+
+    private async Task<decimal> GetBalanceAsync(Guid accountId)
+    {
+        var response = await _client.GetAsync($"/api/accounts/{accountId}/balance");
+        response.EnsureSuccessStatusCode();
+        var result = await response.Content.ReadFromJsonAsync<MbResult<decimal>>();
+        if (result is null)
+            throw new InvalidOperationException("Ошибка при получение баланса в GetBalanceAsync.");
+
+        return result.Value;
+    }
+}
---
+++ BankAccount/BankAccount.Tests/TestApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿using BankAccount.API;
+using BankAccount.DataAccess;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace BankAccount.Tests;
+
+public class TestApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private PostgreSqlContainer _postgreSqlContainer;
+    private string ConnectionString => _postgreSqlContainer.GetConnectionString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseNpgsql(ConnectionString);
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            db.Database.Migrate();
+
+            services.AddAuthentication("Test")
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                    "Test", _ => { });
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        _postgreSqlContainer = new PostgreSqlBuilder()
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _postgreSqlContainer.StopAsync();
+        await _postgreSqlContainer.DisposeAsync();
+    }
+}
